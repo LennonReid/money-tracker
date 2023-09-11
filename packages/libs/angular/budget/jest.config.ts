569a44c1389f208e5e9ed44de1419cdb6c5b191c
Lennon Reid <lenonrade@163.com>
2023-09-11 21:15:08 +0800

    chore: [nx migration] update-configs-jest-29

--- packages/libs/angular/budget/jest.config.ts
+++ packages/libs/angular/budget/jest.config.ts
@@ -3,15 +3,16 @@ export default {
   displayName: 'libs-angular-budget',
   preset: '../../../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
-  globals: {
-    'ts-jest': {
-      tsconfig: '<rootDir>/tsconfig.spec.json',
-      stringifyContentPathRegex: '\\.(html|svg)$',
-    },
-  },
+  globals: {},
   coverageDirectory: '../../../../coverage/packages/libs/angular/budget',
   transform: {
-    '^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular',
+    '^.+\\.(ts|mjs|js|html)$': [
+      'jest-preset-angular',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+        stringifyContentPathRegex: '\\.(html|svg)$',
+      },
+    ],
   },
   transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
   snapshotSerializers: [
